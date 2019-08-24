931ebba645b0a542a1be0d556f02e63c7d624195
Rafael Santos <rafaelnsantos1@gmail.com>
2019-08-24 00:21:06 -0300

    add sort

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const DataLoader = require('dataloader')
 const ObjectId = require('mongodb').ObjectID
 const infoToProjection = require('infotoprojection')
 
-const Loader = (model, info, key = '_id') =>
+const Loader = (model, info, key = '_id', sort = { _id: -1 }) =>
   new DataLoader(async ids =>
     model
       .find(
         { [key]: { $in: key == '_id' ? ids.map(id => ObjectId(id)) : ids } },
         infoToProjection(info)
       )
-      .sort({ _id: -1 })
+      .sort(sort)
   )
 
 module.exports = function (schema) {
-  schema.statics.load = function (id, info, key) {
-    return Loader(this, info, key).load(id)
+  schema.statics.load = function (id, info, key, sort) {
+    return Loader(this, info, key, sort).load(id)
   }
 
-  schema.statics.loadMany = function (ids, info, key) {
-    return Loader(this, info, key).loadMany(ids)
+  schema.statics.loadMany = function (ids, info, key, sort) {
+    return Loader(this, info, key, sort).loadMany(ids)
   }
 }
