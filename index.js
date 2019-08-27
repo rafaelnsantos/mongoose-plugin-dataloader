f887bee7f8f0fb8aaade5250bddb7653c455ea7b
Rafael Santos <rafaelnsantos1@gmail.com>
2019-08-27 02:08:46 -0300

    unused

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const DataLoader = require('dataloader')
 const ObjectId = require('mongodb').ObjectID
 const infoToProjection = require('infotoprojection')
 
-const Loader = (model, info, key = '_id', sort = { _id: -1 }) =>
+const Loader = (model, info, key = '_id') =>
   new DataLoader(ids =>
     model
       .find(
         { [key]: { $in: key == '_id' ? ids.map(id => ObjectId(id)) : ids } },
         infoToProjection(info)
       )
-      .sort(sort)
   )
 
 module.exports = function (schema) {
-  schema.statics.load = function (id, info, key, sort) {
-    return Loader(this, info, key, sort).load(id)
+  schema.statics.load = function (id, info, key) {
+    return Loader(this, info, key).load(id)
   }
 
-  schema.statics.loadMany = function (ids, info, key, sort) {
-    return Loader(this, info, key, sort).loadMany(ids)
+  schema.statics.loadMany = function (ids, info, key) {
+    return Loader(this, info, key).loadMany(ids)
   }
 }
